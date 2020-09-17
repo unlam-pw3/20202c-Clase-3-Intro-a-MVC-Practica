401a6e28798b9e6008d644451ada79de8ba6b02a
Nicolas Diaz Velar <e.diazvelar@gmail.com>
2020-09-17 08:55:48 -0300

    creo AlimentosController con sus respectivos metodos (vacios)

---
+++ PracticaMVC/IntroMVC/Controllers/AlimentosController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class AlimentosController : Controller
+    {
+        // GET: Alimentos/todos
+        public ActionResult Todos()
+        {
+            return View();
+        }
+
+        
+
+        // GET: Alimentos/Alta
+        public ActionResult Alta()
+        {
+            return View();
+        }
+
+        // POST: Alimentos/Crear
+        [HttpPost]
+        public ActionResult Crear(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alimentos/Editar/5
+        public ActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        // POST: Alimentos/Editar/5
+        [HttpPost]
+        public ActionResult Editar(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alimentos/Borrar/5
+        public ActionResult Borrar(int id)
+        {
+            return View();
+        }
+
+        // POST: Alimentos/Borrar/5
+        [HttpPost]
+        public ActionResult Borrar(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
